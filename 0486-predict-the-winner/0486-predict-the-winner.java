ee31314964a54251367eec220590c6e96b9f53ce
Shouvik Pradhan <35471259+shouvik126@users.noreply.github.com>
2026-08-01 22:32:45 +0530

    Time: 2 ms (37.4%), Space: 43.1 MB (5.44%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.java
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -2,20 +2,28 @@ class Solution {
     public boolean predictTheWinner(int[] nums) {
         int n = nums.length;
         int sum = Arrays.stream(nums).sum();
-        int p1 = solve(0, n - 1, nums);
+        int[][] dp = new int[n][n];
+        for (int[] d : dp) {
+            Arrays.fill(d, -1);
+        }
+        int p1 = solve(0, n - 1, nums, dp);
         int p2 = sum - p1;
         return p1 >= p2;
     }
 
-    int solve (int i, int j, int[] nums) {
+    int solve (int i, int j, int[] nums, int[][] dp) {
+        
         if (i > j) {
             return 0;
         }
         if (i == j) {
             return nums[i];
         }
-        int take_i = nums[i] + Math.min(solve(i + 2, j, nums), solve(i + 1, j - 1, nums));
-        int take_j = nums[j] + Math.min(solve(i + 1, j - 1, nums), solve(i, j - 2, nums));
-        return Math.max(take_i, take_j);
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
+        int take_i = nums[i] + Math.min(solve(i + 2, j, nums, dp), solve(i + 1, j - 1, nums, dp));
+        int take_j = nums[j] + Math.min(solve(i + 1, j - 1, nums, dp), solve(i, j - 2, nums, dp));
+        return dp[i][j] = Math.max(take_i, take_j);
     }
 }
