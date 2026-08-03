0780981e2b04bb7a1a653bb3a368b1535e7c1318
Shouvik Pradhan <35471259+shouvik126@users.noreply.github.com>
2026-08-03 22:00:46 +0530

    Time: 3 ms (99.15%), Space: 81.3 MB (96.87%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.java
+++ 1406-stone-game-iii/1406-stone-game-iii.java
@@ -31,7 +31,7 @@
     }
 } */
 
-class Solution {
+/* class Solution {
     public String stoneGameIII(int[] stoneValue) {
         int n = stoneValue.length;
         int[] dp = new int[n + 1];
@@ -47,6 +47,36 @@ class Solution {
         if (diff > 0) {
             return "Alice";
         } else if (diff < 0) {
+            return "Bob";
+        } else {
+            return "Tie";
+        }
+    }
+} */
+
+class Solution {
+    public String stoneGameIII(int[] stoneValue) {
+        int n = stoneValue.length;
+
+        int a = 0;
+        int b = 0;
+        int c = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            int result = Integer.MIN_VALUE;
+            result = stoneValue[i] - a;
+            if (i + 2 <= n)
+                result = Math.max(result, stoneValue[i] + stoneValue[i + 1] - b);
+
+            if (i + 3 <= n)
+                result = Math.max(result, (stoneValue[i] + stoneValue[i + 1] + stoneValue[i + 2] - c));
+            
+            c = b;
+            b = a;
+            a = result;
+        }
+        if (a > 0) {
+            return "Alice";
+        } else if (a < 0) {
             return "Bob";
         } else {
             return "Tie";
