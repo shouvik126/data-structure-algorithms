67da80d1ef3c5969b62304e005b84336ee2506b5
Shouvik Pradhan <35471259+shouvik126@users.noreply.github.com>
2026-08-10 18:32:46 +0530

    Time: 19 ms (57.18%), Space: 42.5 MB (68.41%) - LeetHub

--- 1510-stone-game-iv/1510-stone-game-iv.java
+++ 1510-stone-game-iv/1510-stone-game-iv.java
@@ -1,24 +1,40 @@
+// class Solution {
+//     public boolean winnerSquareGame(int n) {
+//         int[] dp = new int[n + 1];
+//         Arrays.fill(dp, -1);
+//         return solve(n, dp);
+//     }
+
+//     public boolean solve(int n, int[] dp) {
+//         if (n == 0) {
+//             return false;
+//         }
+//         if (dp[n] != -1) {
+//             return dp[n] == 1 ? true : false;
+//         }
+//         for (int k = 1; k * k <= n; k++) {
+//             if (!solve(n - (k * k), dp)) {
+//                 dp[n] = 1;
+//                 return true;
+//             }
+//         }
+//         dp[n] = 0;
+//         return false;
+//     }
+// }
+
+// Bottom Up
 class Solution {
     public boolean winnerSquareGame(int n) {
-        int[] dp = new int[n + 1];
-        Arrays.fill(dp, -1);
-        return solve(n, dp);
-    }
-
-    public boolean solve(int n, int[] dp) {
-        if (n == 0) {
-            return false;
-        }
-        if (dp[n] != -1) {
-            return dp[n] == 1 ? true : false;
-        }
-        for (int k = 1; k * k <= n; k++) {
-            if (!solve(n - (k * k), dp)) {
-                dp[n] = 1;
-                return true;
+        boolean[] dp = new boolean[n + 1];
+        for (int i = 1; i <= n; i++) {
+            for (int k = 1; k*k <= i; k++) {
+                if (!dp[i - (k * k)]) {
+                    dp[i] = true;
+                    break;
+                }
             }
         }
-        dp[n] = 0;
-        return false;
+        return dp[n];
     }
 }
