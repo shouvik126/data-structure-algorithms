8c98f93ee24e657b3c75a48f886b3b8bebb78bb2
Shouvik Pradhan <35471259+shouvik126@users.noreply.github.com>
2026-08-17 02:11:44 +0530

    Time: 7 ms (29.14%), Space: 114.8 MB (52.32%) - LeetHub

---
+++ 2029-stone-game-ix/2029-stone-game-ix.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean stoneGameIX(int[] stones) {
+        int c0 = 0;
+        int c1 = 0;
+        int c2 = 0;
+        for (int stone : stones) {
+            if (stone % 3 == 0) {
+                c0++;
+            } else if (stone % 3 == 1) {
+                c1++;
+            } else {
+                c2++;
+            }
+        }
+
+        if (c0 % 2 == 0) {
+            return (c1 >= 1 && c2 >= 1) && (c1 >= c2 || c2 >= c1);
+        }
+        return Math.abs(c1 - c2) >= 3;
+
+    }
+}
