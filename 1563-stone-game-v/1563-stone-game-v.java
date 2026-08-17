a8ac3102a393395df0c8832f0a7e047fb802b8f7
Shouvik Pradhan <35471259+shouvik126@users.noreply.github.com>
2026-08-17 19:06:43 +0530

    Time: 352 ms (15.62%), Space: 47.7 MB (60.58%) - LeetHub

--- 1563-stone-game-v/1563-stone-game-v.java
+++ 1563-stone-game-v/1563-stone-game-v.java
@@ -1,7 +1,7 @@
 //Approach-1 - Recursion + Memo
 //T.C : O(n^3)
 //S.C : O(n^2)
-class Solution {
+/* class Solution {
     int[][] dp;
     int n;
     public int stoneGameV(int[] stoneValue) {
@@ -39,5 +39,40 @@ class Solution {
             }
         }
         return dp[l][r] = score;
+    }
+} */
+
+//Approach-2 - Bottom Up (Tabulation)
+//T.C : O(n^3)
+//S.C : O(n^2)
+
+class Solution {
+    public int stoneGameV(int[] stoneValue) {
+        int n = stoneValue.length;
+        int[] cumSum = new int[n];
+        cumSum[0] = stoneValue[0];
+        for (int i = 1; i < n; i++) {
+            cumSum[i] = cumSum[i - 1] + stoneValue[i];
+        }
+        int[][] t = new int[n+1][n+1];
+        for(int l = n-1; l >= 0; l--) {
+            for(int r = l+1; r < n; r++) {
+                //l...r
+                int score = 0;
+                for(int mid = l; mid <= r-1; mid++) {
+                    int leftSum  = cumSum[mid] - (l-1 >= 0 ? cumSum[l-1] : 0); //[l..mid]
+                    int rightSum = cumSum[r] - cumSum[mid]; //mid+1, r
+                    if(leftSum < rightSum) {
+                        score = Math.max(score, leftSum + t[l][mid]);
+                    } else if(leftSum > rightSum) {
+                        score = Math.max(score, rightSum + t[mid+1][r]);
+                    } else {
+                        score = Math.max(score, Math.max(leftSum + t[l][mid], rightSum + t[mid+1][r]));
+                    }
+                }
+                t[l][r] = score;
+            }
+        }
+        return t[0][n-1];
     }
 }
