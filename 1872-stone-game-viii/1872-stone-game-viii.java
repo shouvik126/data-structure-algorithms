cee9830dc47b5f12d1b30dfefeee1be6be4ef0ab
Shouvik Pradhan <35471259+shouvik126@users.noreply.github.com>
2026-08-24 14:37:24 +0530

    Time: 5 ms (42.98%), Space: 86.7 MB (72.34%) - LeetHub

--- 1872-stone-game-viii/1872-stone-game-viii.java
+++ 1872-stone-game-viii/1872-stone-game-viii.java
@@ -1,3 +1,40 @@
+//Approach-1 - Recursion + Memo
+//T.C : O(n)
+//S.C : O(n)
+// class Solution {
+//     int n;
+//     int[] dp;
+//     int[] prefSum;
+//     boolean[] visited;
+//     public int stoneGameVIII(int[] stones) {
+//         n = stones.length;
+//         dp = new int[n + 1];
+//         visited = new boolean[n + 1];
+//         Arrays.fill(dp, -1);
+//         prefSum = new int[n];
+//         prefSum[0] = stones[0];
+//         for (int i = 1; i < n; i++) {
+//             prefSum[i] = prefSum[i - 1] + stones[i];
+//         }
+//         return solve(1);
+//     }
+//     public int solve(int i) {
+//         if (i == n - 1) {
+//             return prefSum[i];
+//         }
+//         if (visited[i]) {
+//             return dp[i];
+//         }
+//         visited[i] = true;
+//         int takei = prefSum[i] - solve(i + 1);
+//         int skipi = solve(i + 1);
+//         return dp[i] = Math.max(takei, skipi);
+//     }
+// }
+
+//Approach-2 - Bottom Up
+//T.C : O(n)
+//S.C : O(n)
 class Solution {
     int n;
     int[] dp;
@@ -6,25 +43,18 @@ class Solution {
     public int stoneGameVIII(int[] stones) {
         n = stones.length;
         dp = new int[n + 1];
-        visited = new boolean[n + 1];
-        Arrays.fill(dp, -1);
         prefSum = new int[n];
         prefSum[0] = stones[0];
         for (int i = 1; i < n; i++) {
             prefSum[i] = prefSum[i - 1] + stones[i];
         }
-        return solve(1);
-    }
-    public int solve(int i) {
-        if (i == n - 1) {
-            return prefSum[i];
+        dp[n - 1] = prefSum[n - 1];
+        for (int i = n - 2; i >= 1; i--) {
+            int takei = prefSum[i] - dp[i + 1];
+            int skipi = dp[i + 1];
+            
+            dp[i] = Math.max(takei, skipi);
         }
-        if (visited[i]) {
-            return dp[i];
-        }
-        visited[i] = true;
-        int takei = prefSum[i] - solve(i + 1);
-        int skipi = solve(i + 1);
-        return dp[i] = Math.max(takei, skipi);
+        return dp[1];
     }
 }
