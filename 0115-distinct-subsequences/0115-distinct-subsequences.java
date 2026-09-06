f60d436d57d44648125a1215da9c8a698761e59a
Shouvik Pradhan <35471259+shouvik126@users.noreply.github.com>
2026-09-06 15:22:21 +0530

    Time: 26 ms (9%), Space: 138.1 MB (6.38%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,28 +1,57 @@
+//Approach-1 (Recursion + MEmoization)
+//T.C : O(m*n)
+//S.C : O(m*n)
+// class Solution {
+//     public int numDistinct(String s, String t) {
+//         int m = s.length();
+//         int n = t.length();
+//         int[][] dp = new int[1001][1001];
+//         for (int[] d : dp) {
+//             Arrays.fill(d, -1);
+//         }
+//         return solve(s, t, m, n, dp);
+//     }
+//     public int solve(String s, String t, int m, int n, int[][] dp) {
+//         if (n == 0) {
+//             return 1;
+//         }
+//         if (m == 0) {
+//             return 0;
+//         }
+//         if (dp[m][n] != -1) {
+//             return dp[m][n];
+//         }
+
+//         if (s.charAt(m - 1) == t.charAt(n - 1)) {
+//             return dp[m][n] = solve(s, t, m - 1, n - 1, dp) + solve(s, t, m - 1, n, dp);
+//         } else {
+//             return dp[m][n] = solve(s, t, m - 1, n, dp);
+//         }
+//     }
+// }
+
+
 class Solution {
     public int numDistinct(String s, String t) {
         int m = s.length();
         int n = t.length();
         int[][] dp = new int[1001][1001];
-        for (int[] d : dp) {
-            Arrays.fill(d, -1);
+        for (int i = 0; i <= m; i++) {
+            dp[i][0] = 1;
         }
-        return solve(s, t, m, n, dp);
+        for (int j = 1; j <= n; j++) {
+            dp[0][j] = 0;
+        }
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[m][n];
     }
-    public int solve(String s, String t, int m, int n, int[][] dp) {
-        if (n == 0) {
-            return 1;
-        }
-        if (m == 0) {
-            return 0;
-        }
-        if (dp[m][n] != -1) {
-            return dp[m][n];
-        }
-
-        if (s.charAt(m - 1) == t.charAt(n - 1)) {
-            return dp[m][n] = solve(s, t, m - 1, n - 1, dp) + solve(s, t, m - 1, n, dp);
-        } else {
-            return dp[m][n] = solve(s, t, m - 1, n, dp);
-        }
-    }
+    
 }
