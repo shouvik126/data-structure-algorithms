7462f31a90477ff3c7d4b5adeff8415c221dab35
Shouvik Pradhan <35471259+shouvik126@users.noreply.github.com>
2026-09-06 15:52:01 +0530

    Time: 18 ms (76.88%), Space: 46.9 MB (83.94%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -31,27 +31,54 @@
 // }
 
 
+
+//Approach-2 (Bottom UP DP)
+//T.C : O(m*n)
+//S.C : O(m*n)
+// class Solution {
+//     public int numDistinct(String s, String t) {
+//         int m = s.length();
+//         int n = t.length();
+//         int[][] dp = new int[1001][1001];
+//         for (int i = 0; i <= m; i++) {
+//             dp[i][0] = 1;
+//         }
+//         for (int j = 1; j <= n; j++) {
+//             dp[0][j] = 0;
+//         }
+//         for (int i = 1; i <= m; i++) {
+//             for (int j = 1; j <= n; j++) {
+//                 if (s.charAt(i - 1) == t.charAt(j - 1)) {
+//                     dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+//                 } else {
+//                     dp[i][j] = dp[i - 1][j];
+//                 }
+//             }
+//         }
+//         return dp[m][n];
+//     }
+    
+// }
+
 class Solution {
     public int numDistinct(String s, String t) {
         int m = s.length();
         int n = t.length();
-        int[][] dp = new int[1001][1001];
-        for (int i = 0; i <= m; i++) {
-            dp[i][0] = 1;
-        }
-        for (int j = 1; j <= n; j++) {
-            dp[0][j] = 0;
-        }
+        int[] prev = new int[1001];
+        int[] curr = new int[1001];
+        prev[0] = 1;
+        curr[0] = 1;
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= n; j++) {
                 if (s.charAt(i - 1) == t.charAt(j - 1)) {
-                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                    curr[j] = prev[j - 1] + prev[j];
                 } else {
-                    dp[i][j] = dp[i - 1][j];
+                    curr[j] = prev[j];
                 }
             }
+            prev = curr.clone();
         }
-        return dp[m][n];
+        return prev[n];
     }
     
 }
