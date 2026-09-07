64fbaaa4e26b1df33d630bcfd116e5b54786dd01
Shouvik Pradhan <35471259+shouvik126@users.noreply.github.com>
2026-09-07 19:57:13 +0530

    Time: 5 ms (54.61%), Space: 45 MB (19.15%) - LeetHub

--- 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.java
+++ 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.java
@@ -1,3 +1,43 @@
+//Approach-1 (Recursion Memo with duplicate handling)
+//T.C : O(n)
+//S.C : O(n)
+// class Solution {
+//     int M = 1_000_000_007;
+//     int[] prev = new int[2001]; //prev[n] = last time when we saw this nth character (1-based indexing)
+//     int[] dp = new int[2001];
+//     public int distinctSubseqII(String s) {
+//         int n = s.length();
+        
+//         Arrays.fill(dp, -1);
+        
+//         int[] lastSeen = new int[26];
+//         for (int i = 1; i <= n; i++) {
+//             int idx = s.charAt(i - 1) - 'a';
+//             prev[i] = lastSeen[idx];
+//             lastSeen[idx] = i;
+//         }
+//         return (solve(n) - 1 + M) % M;
+//     }
+
+//     public int solve(int n) {
+//         if (n == 0) {
+//             return 1;
+//         }
+//         if (dp[n] != -1) {
+//             return dp[n];
+//         }
+//         int total = (solve(n - 1) * 2) % M;
+//         if (prev[n] != 0) {
+//             int diff = (solve(prev[n] - 1)) % M;
+//             total = (total - diff + M) % M;
+//         }
+//         return dp[n] = total;
+//     }
+// }
+
+//Approach-2 (Bottom Up with duplicate handling)
+//T.C : O(n)
+//S.C : O(n)
 class Solution {
     int M = 1_000_000_007;
     int[] prev = new int[2001]; //prev[n] = last time when we saw this nth character (1-based indexing)
@@ -13,21 +53,17 @@ class Solution {
             prev[i] = lastSeen[idx];
             lastSeen[idx] = i;
         }
-        return (solve(n) - 1 + M) % M;
+
+        dp[0] = 1;
+        for (int i = 1; i <= n; i++) {
+            int total = (dp[i - 1] * 2) % M;
+            if (prev[i] != 0) {
+                int diff = (dp[prev[i] - 1]) % M;
+                total = (total - diff + M) % M;
+            }
+            dp[i] = total;
+        }
+        return (dp[n] - 1 + M) % M;
     }
 
-    public int solve(int n) {
-        if (n == 0) {
-            return 1;
-        }
-        if (dp[n] != -1) {
-            return dp[n];
-        }
-        int total = (solve(n - 1) * 2) % M;
-        if (prev[n] != 0) {
-            int diff = (solve(prev[n] - 1)) % M;
-            total = (total - diff + M) % M;
-        }
-        return dp[n] = total;
-    }
 }
