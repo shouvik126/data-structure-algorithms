4181e1734e13a17e96276ef12e09f33768c02d5b
Shouvik Pradhan <35471259+shouvik126@users.noreply.github.com>
2026-09-09 22:21:15 +0530

    Time: 1 ms (99.36%), Space: 42.6 MB (71.15%) - LeetHub

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
@@ -1,18 +1,33 @@
 // TC: O(log(base 1000)(n))
 // SC: O(1)
+// class Solution {
+//     public long countCommas(long n) {
+//         long result = 0;
+//         long lower = 1000;
+//         long comm = 1;
+//         while (lower <= n) {
+//             long upper = (lower * 1000) - 1;
+//             if (upper  > n)
+//                 upper = n;
+//             long total = (upper - lower) + 1;
+//             result += total * comm;
+//             comm++;
+//             lower *= 1000;
+//         }
+//         return result;
+//     }
+// }
+
+// Approach 2
+// TC: O(log(base 1000)(n))
+// SC: O(1)
 class Solution {
     public long countCommas(long n) {
         long result = 0;
-        long lower = 1000;
-        long comm = 1;
-        while (lower <= n) {
-            long upper = (lower * 1000) - 1;
-            if (upper  > n)
-                upper = n;
-            long total = (upper - lower) + 1;
-            result += total * comm;
-            comm++;
-            lower *= 1000;
+        long start = 1000;
+        while (start <= n) {
+            result += (n - start + 1);
+            start *= 1000;
         }
         return result;
     }
