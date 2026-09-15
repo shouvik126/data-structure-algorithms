c0fa4d152f132aa228f67c778b21358b00127966
Shouvik Pradhan <35471259+shouvik126@users.noreply.github.com>
2026-09-15 21:48:21 +0530

    Time: 184 ms (11.06%), Space: 60.4 MB (45.23%) - LeetHub

--- 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.java
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.java
@@ -136,37 +136,63 @@
 //     }
 // }
 
-//Approach-4 (Bottom Up of Approach-1 Above)
-//T.C : O(n^3)
+//Approach-4 (Using Blueprint of Palindrome Strings to convert isPalindrome check to O(1))
+//T.C : O(n^2)
+//S.C : O(n^2)
+// class Solution {
+//     int [][]dp;
+//     boolean[][] isPalindrome;
+//     public int maxPalindromes(String s, int k) {
+//         int n = s.length();
+//         if (k == 1)
+//             return n;
+//         dp = new int[n + 1][n + 1];
+//         isPalindrome = new boolean[n + 1][n + 1];
+//         generateBluePrint(s, n);
+//         dp[n][n] = 0; // base case
+//         for (int i = n - 1; i >= 0; i--) {
+//             for  (int j = n - 1; j >= 0; j--) {
+//                 if (isPalindrome[i][j]) {
+//                     int take = 1 + (j + k <= n ? dp[j + 1][j + k] : 0);
+//                     int grow = dp[i][j + 1];
+//                     int shift = dp[i + 1][j + 1];
+//                     dp[i][j] = Math.max(take, Math.max(grow, shift));
+//                 } else {
+//                     int grow = dp[i][j + 1];
+//                     int shift = dp[i + 1][j + 1];
+//                     dp[i][j] = Math.max(grow, shift);
+//                 }
+//             }
+//         }
+//         return dp[0][k - 1];
+//     }
+//     public void generateBluePrint(String s, int n) {
+//         for (int L = 1; L <= n; L++) {
+//             for (int i = 0; i + L - 1 < n; i++) {
+//                 int j = i + L - 1;
+//                 if (i == j) {
+//                     isPalindrome[i][j] = true;
+//                 } else if (i + 1 == j) {
+//                     isPalindrome[i][j] = s.charAt(i) == s.charAt(j);
+//                 } else {
+//                     isPalindrome[i][j] = s.charAt(i) == s.charAt(j) && isPalindrome[i + 1][j - 1];
+//                 }
+//             }
+//         }
+//     }
+// }
+
+//Approach-5 (Using Blueprint of Palindrome Strings to convert isPalindrome check to O(1))
+//T.C : O(n^2)
 //S.C : O(n^2)
 class Solution {
-    int [][]dp;
+    int []dp;
     boolean[][] isPalindrome;
     public int maxPalindromes(String s, int k) {
         int n = s.length();
-        if (k == 1)
-            return n;
-        dp = new int[n + 1][n + 1];
+        dp = new int[n + 1];
+        Arrays.fill(dp, -1);
         isPalindrome = new boolean[n + 1][n + 1];
-        generateBluePrint(s, n);
-        dp[n][n] = 0; // base case
-        for (int i = n - 1; i >= 0; i--) {
-            for  (int j = n - 1; j >= 0; j--) {
-                if (isPalindrome[i][j]) {
-                    int take = 1 + (j + k <= n ? dp[j + 1][j + k] : 0);
-                    int grow = dp[i][j + 1];
-                    int shift = dp[i + 1][j + 1];
-                    dp[i][j] = Math.max(take, Math.max(grow, shift));
-                } else {
-                    int grow = dp[i][j + 1];
-                    int shift = dp[i + 1][j + 1];
-                    dp[i][j] = Math.max(grow, shift);
-                }
-            }
-        }
-        return dp[0][k - 1];
-    }
-    public void generateBluePrint(String s, int n) {
         for (int L = 1; L <= n; L++) {
             for (int i = 0; i + L - 1 < n; i++) {
                 int j = i + L - 1;
@@ -179,5 +205,23 @@ class Solution {
                 }
             }
         }
+        return solve(s, n, k);
+    }
+
+    public int solve(String s, int n, int k) {
+        if (n < k) {
+            return 0;
+        }
+        if (dp[n] != -1) {
+            return dp[n];
+        }
+        int result = solve(s, n - 1, k);
+        int j = n - 1;
+        for (int i = 0; j - i + 1 >= k; i++) {
+            if (isPalindrome[i][j]) {
+                result = Math.max(result, 1 + solve(s, i, k));
+            }
+        }
+        return dp[n] = result;
     }
 }
