c0abb7152878acdad59ade373186a06307faa4f8
Shouvik Pradhan <35471259+shouvik126@users.noreply.github.com>
2026-09-15 19:45:57 +0530

    Time: 29 ms (25.18%), Space: 53.1 MB (7.9%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,7 +1,10 @@
 class Solution {
+    int[][] dp;
     public int countSubstrings(String s) {
         int n = s.length();
         int count = 0;
+        dp = new int[n + 1][n + 1];
+        for (int[] d : dp) Arrays.fill(d, -1);
         for (int i = 0; i < n; i++) {
             for (int j = i; j < n; j++) {
                 if (check(s, i, j)) {
@@ -15,9 +18,14 @@ class Solution {
         if (i > j) {
             return true;
         }
-        if (s.charAt(i) == s.charAt(j)) {
-            return check(s, i + 1, j - 1);
+        if (dp[i][j] != -1) {
+            return dp[i][j] == 1 ? true : false;
         }
+        if (s.charAt(i) == s.charAt(j)) {
+            dp[i][j] = check(s, i + 1, j - 1) == true ? 1 : 0;
+            return dp[i][j] == 1 ? true : false;
+        }
+        dp[i][j] = 0;
         return false;
     }
 }
