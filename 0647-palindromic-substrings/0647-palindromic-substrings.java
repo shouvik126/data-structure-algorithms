f4babe84192cc7f1335bb07e859d533d2df4cfd1
Shouvik Pradhan <35471259+shouvik126@users.noreply.github.com>
2026-09-15 20:50:02 +0530

    Time: 21 ms (28.59%), Space: 46.8 MB (18.15%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,31 +1,58 @@
+// class Solution {
+//     int[][] dp;
+//     public int countSubstrings(String s) {
+//         int n = s.length();
+//         int count = 0;
+//         dp = new int[n + 1][n + 1];
+//         for (int[] d : dp) Arrays.fill(d, -1);
+//         for (int i = 0; i < n; i++) {
+//             for (int j = i; j < n; j++) {
+//                 if (check(s, i, j)) {
+//                     count++;
+//                 }
+//             }
+//         }
+//         return count;
+//     }
+//     public boolean check(String s, int i, int j) {
+//         if (i > j) {
+//             return true;
+//         }
+//         if (dp[i][j] != -1) {
+//             return dp[i][j] == 1 ? true : false;
+//         }
+//         if (s.charAt(i) == s.charAt(j)) {
+//             dp[i][j] = check(s, i + 1, j - 1) == true ? 1 : 0;
+//             return dp[i][j] == 1 ? true : false;
+//         }
+//         dp[i][j] = 0;
+//         return false;
+//     }
+// }
+
+
+// Approach: 2
 class Solution {
-    int[][] dp;
+    boolean[][] dp;
     public int countSubstrings(String s) {
         int n = s.length();
         int count = 0;
-        dp = new int[n + 1][n + 1];
-        for (int[] d : dp) Arrays.fill(d, -1);
-        for (int i = 0; i < n; i++) {
-            for (int j = i; j < n; j++) {
-                if (check(s, i, j)) {
+        dp = new boolean[n + 1][n + 1];
+        for (int L = 1; L <= n; L++) {
+            for (int i = 0; i+L-1 < n; i++) {
+                int j = i + L - 1;
+                if (i == j) {
+                    dp[i][j] = true;
+                } else if (i + 1 == j) {
+                    dp[i][j] = s.charAt(i) == s.charAt(j);
+                } else {
+                    dp[i][j] = s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1];
+                }
+                if (dp[i][j]) {
                     count++;
                 }
             }
         }
         return count;
-    }
-    public boolean check(String s, int i, int j) {
-        if (i > j) {
-            return true;
-        }
-        if (dp[i][j] != -1) {
-            return dp[i][j] == 1 ? true : false;
-        }
-        if (s.charAt(i) == s.charAt(j)) {
-            dp[i][j] = check(s, i + 1, j - 1) == true ? 1 : 0;
-            return dp[i][j] == 1 ? true : false;
-        }
-        dp[i][j] = 0;
-        return false;
     }
 }
