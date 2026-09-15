4979819ce05a4a5363ea9646fed6322a9903a588
Shouvik Pradhan <35471259+shouvik126@users.noreply.github.com>
2026-09-15 19:09:52 +0530

    Time: 2074 ms (5.03%), Space: 183.3 MB (9.55%) - LeetHub

--- 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.java
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.java
@@ -1,6 +1,52 @@
 //Approach-1 (Simple Recursion Memoization)
 //T.C : O(n^3)
 //S.C : O(n^2)
+// class Solution {
+//     int [][]dp;
+//     public int maxPalindromes(String s, int k) {
+//         int n = s.length();
+//         if (k == 1)
+//             return n;
+//         dp = new int[n + 1][n + 1];
+//         for (int []d : dp) {
+//             Arrays.fill(d, -1);
+//         }
+//         return solve(s, k, 0, k - 1);
+//     }
+
+//     public int solve(String s, int k, int i, int j) {
+//         if (i >= s.length() || j >= s.length()) {
+//             return 0;
+//         }
+//         if (dp[i][j] != -1) {
+//             return dp[i][j];
+//         }
+//         if (isPelindrome(s, i, j)) {
+//             int take = 1 + solve(s, k, j + 1, j + k);
+//             int grow = solve(s, k, i, j + 1);
+//             int shift = solve(s, k, i + 1, j + 1);
+//             return dp[i][j] = Math.max(take, Math.max(grow, shift));
+//         } else {
+//             int grow = solve(s, k, i, j + 1);
+//             int shift = solve(s, k, i + 1, j + 1);
+//             return dp[i][j] = Math.max(grow, shift);
+//         }
+//     }
+//     public boolean isPelindrome(String s, int i, int j) {
+//         while (i <= j) {
+//             if (s.charAt(i) != s.charAt(j)) {
+//                 return false;
+//             }
+//             i++;
+//             j--;
+//         }
+//         return true;
+//     }
+// }
+
+//Approach-2 (Bottom Up of Approach-1 Above)
+//T.C : O(n^3)
+//S.C : O(n^2)
 class Solution {
     int [][]dp;
     public int maxPalindromes(String s, int k) {
@@ -8,29 +54,22 @@ class Solution {
         if (k == 1)
             return n;
         dp = new int[n + 1][n + 1];
-        for (int []d : dp) {
-            Arrays.fill(d, -1);
+        dp[n][n] = 0; // base case
+        for (int i = n - 1; i >= 0; i--) {
+            for  (int j = n - 1; j >= i; j--) {
+                if (isPelindrome(s, i, j)) {
+                    int take = 1 + (j + k <= n ? dp[j + 1][j + k] : 0);
+                    int grow = dp[i][j + 1];
+                    int shift = dp[i + 1][j + 1];
+                    dp[i][j] = Math.max(take, Math.max(grow, shift));
+                } else {
+                    int grow = dp[i][j + 1];
+                    int shift = dp[i + 1][j + 1];
+                    dp[i][j] = Math.max(grow, shift);
+                }
+            }
         }
-        return solve(s, k, 0, k - 1);
-    }
-
-    public int solve(String s, int k, int i, int j) {
-        if (i >= s.length() || j >= s.length()) {
-            return 0;
-        }
-        if (dp[i][j] != -1) {
-            return dp[i][j];
-        }
-        if (isPelindrome(s, i, j)) {
-            int take = 1 + solve(s, k, j + 1, j + k);
-            int grow = solve(s, k, i, j + 1);
-            int shift = solve(s, k, i + 1, j + 1);
-            return dp[i][j] = Math.max(take, Math.max(grow, shift));
-        } else {
-            int grow = solve(s, k, i, j + 1);
-            int shift = solve(s, k, i + 1, j + 1);
-            return dp[i][j] = Math.max(grow, shift);
-        }
+        return dp[0][k - 1];
     }
     public boolean isPelindrome(String s, int i, int j) {
         while (i <= j) {
