61e5b6e8dd695369af959a482619bdb3e5d7bdeb
Shouvik Pradhan <35471259+shouvik126@users.noreply.github.com>
2026-09-19 16:47:27 +0530

    Time: 0 ms (100%), Space: 42.4 MB (22.69%) - LeetHub

--- 1401-circle-and-rectangle-overlapping/1401-circle-and-rectangle-overlapping.java
+++ 1401-circle-and-rectangle-overlapping/1401-circle-and-rectangle-overlapping.java
@@ -1,3 +1,6 @@
+//Approach-1 (Nearest point check and compare)
+//T.C : O(1)
+//S.C : O(1)
 class Solution {
     public boolean checkOverlap(int radius, int xCenter, int yCenter, int x1, int y1, int x2, int y2) {
         int xi = Integer.MAX_VALUE;
